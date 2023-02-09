be440e818f4752fde457c26ed3fc90423521dcdf
nonthask laoluerat <nonthasak@Macintosh.local>
2023-02-09 13:08:32 +0700

    update: move trasfer event to Once event of the client, update intets bits

--- src/events/transfer.ts
+++ src/events/transfer.ts
@@ -45,4 +45,4 @@ async function specialAvoidChecking(address: string) {
   }
 }
 
-transferEventHandler();
+export { transferEventHandler };
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,21 @@
 import "./discord/verify.handler";
-import "./events/transfer";
+import { transferEventHandler } from "./events/transfer";
 import * as dotenv from "dotenv";
 import { Client, Events, GatewayIntentBits } from "discord.js";
 dotenv.config();
 const { discord_token } = process.env;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.GuildMessageReactions,
+  ],
+});
 
-client.once(Events.ClientReady, (data) => {
+client.once(Events.ClientReady, async (data) => {
+  await transferEventHandler();
   console.log(`XRB ready !  => ${data.user.tag}`);
 });
 
